88d88ff9d4ca9a1def51d55a39394e6d9c783fd6
SusanaArtime <susartime@gmail.com>
2024-09-20 13:38:41 +0200

    añadidos ejercicio 2 y 3.los he resuelto de la misma manera

---
+++ ejercicio2.java
@@ -0,0 +1,20 @@
+package CursoJava;
+import java.util.Scanner;
+public class ejercicio2{
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A, B;
+        System.out.print("Introduzca valor de A: ");
+        A = sc.nextInt();
+        System.out.print("Introduzca Valor de B: ");
+        B = sc.nextInt();
+        System.out.println("Valores iniciales: A = " + A + "   B = " + B);                                        
+        
+        A = A + B;
+        B = A - B;
+        A = A - B;
+        System.out.println("Valores intercambiados: A = " + A + "   B = " + B);                                   
+    }
+    
+}
