adecc2d7dc24e2a819af3390d7a2cc3cdf320ff4
SusanaArtime <susartime@gmail.com>
2024-09-20 16:41:00 +0200

    añadido ejercicio9

--- ejercicio9.java
+++ ejercicio9.java
@@ -1,5 +1,15 @@
-package CursoJava;
 
-public class ejercicio9 {
-    
+package CursoJava;
+import javax.swing.JOptionPane;
+
+public class ejercicio9{
+    public static void main(String[] args) {
+        
+        // GUI intro
+        String name = JOptionPane.showInputDialog("Hola!!¿Como te llamas?");
+        JOptionPane.showMessageDialog(null,"Encantada de conocerte " + name); 
+
+                
+     }
 }
+
