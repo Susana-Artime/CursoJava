a2a44e2b5f304e08db0b52fd681b301734480523
SusanaArtime <susartime@gmail.com>
2024-09-22 21:32:10 +0200

    añadido ejercicio20

---
+++ ejercicio20.java
@@ -0,0 +1,35 @@
+//Condiciones lógicas: Crea un programa que verifique si un número es positivo
+//y par usando AND lógico, y si es negativo o divisible por 3 usando OR lógico.
+
+package CursoJava;
+import java.util.Scanner;
+
+public class ejercicio20 {
+     public static void main(String[] args) {
+
+        int num;
+        
+        Scanner sc= new Scanner(System.in);
+
+        System.out.println("Ingresa un número:");
+        num = sc.nextInt();
+
+        if(num>0 & num%2==0){
+
+            System.out.println("El número: " + num + " es positivo y par");
+            
+        }
+        else {
+            if (num<0 || num%3==0){
+                 
+                System.out.println("El número: " + num + " es negativo o divisible por 3");
+
+            }
+        }
+    
+
+        sc.close();
+
+}
+
+}
