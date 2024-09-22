f7ba07c394a82e0ca6802c742a5cc9b51f3b817c
SusanaArtime <susartime@gmail.com>
2024-09-22 19:08:31 +0200

    añadido ejercicio13

---
+++ ejercicio13.java
@@ -0,0 +1,13 @@
+package CursoJava;
+
+public class ejercicio13 {
+    public static void main(String[] args) {
+
+    int dado1;
+    int dado2;
+    dado1 = (int) (Math.random()*6+1);
+    dado2 = (int) (Math.random()*6+1);
+    System.out.println("El primer dado saca " + dado1 + " y el segundo dado saca " + dado2);
+
+    }
+}
