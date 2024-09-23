fa3f161caf14fe03ff42d4db712724721a896b42
SusanaArtime <susartime@gmail.com>
2024-09-23 18:12:08 +0200

    añadido ejercicio23

---
+++ ejercicio23.java
@@ -0,0 +1,27 @@
+//Imprimir tabla de multiplicar: Escribe un programa que imprima la tabla de
+//multiplicar de un número ingresado por el usuario usando un bucle for.
+
+import java.util.Scanner;
+
+public class ejercicio23 {
+
+    public static void main(String[] args) {
+         
+      int num; 
+      int resultado;   
+      Scanner sc = new Scanner(System.in);
+
+      System.out.println("Vamos a imprimir la tabla de multiplicar del número: ");
+      num=sc.nextInt();
+      
+      for(int i=1;i<=10;i++){
+        
+        resultado = i*num;
+        System.out.println(num + " x " +i+ " = " +resultado);
+
+      }
+
+
+    }
+    
+}
